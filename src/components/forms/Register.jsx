aabbd76489fcbeb7e222e28173031d2d89d31747
Your Name <gisdreamer13@gmail.com>
2024-02-13 18:09:15 -0700

    feb13

--- src/components/forms/Register.jsx
+++ src/components/forms/Register.jsx
@@ -5,7 +5,7 @@ export default function Register() {
     const [user, setUser] = useState({ username: '', email: '', password: '' })
 
     async function registerUser(){
-        const res = await fetch('http://127.0.0.1:5000/login',{
+        const res = await fetch('http://127.0.0.1:5000/user',{
             method: "POST",
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify(user)
@@ -25,6 +25,7 @@ export default function Register() {
 
     return (
         <>
+        <div onChange={(e) => setUser({ ...user, password, email })}>
             <h3>Register</h3>
             <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="username">Username</label><br />
@@ -35,6 +36,7 @@ export default function Register() {
                 <input type="password" name='password' value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })}/><br />
                 <input type="Submit" value='Register'/>
             </form>
+        </div>
         </>
     )
 
